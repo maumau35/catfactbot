304e35d7ffd835bde5ead7f4c476617f970dd57e
maumau35 <35198362+maumau35@users.noreply.github.com>
2018-08-31 13:25:39 +0200

    Update catfactbot.py

--- catfactbot.py
+++ catfactbot.py
@@ -33,31 +33,17 @@ for comment in comments:
                 'kitty' not in comment.body and
                 'suicide' not in comment.body and
                 'died' not in comment.body):
-                if author.name not in done:
+                if author.name not in done and author.name not in weird:
                     if comment.subreddit not in ["depression","suicidewatch","pcmasterrace"]:
                         comment.body=comment.body.split("\n")
                         print("replying to {0}'s (catfact) comment: {1}".format(author.name, comment.body))
-                        s1 = (requests.get('https://catfact.ninja/fact').json())
-                        s1 = str(s1)
-                        def between(value, a, b):
-    # Find and validate before-part.
-                            pos_a = value.find(a)
-                            if pos_a == -1: return ""
-    # Find and validate after part.
-                            pos_b = value.rfind(b)
-                            if pos_b == -1: return ""
-    # Return middle part.
-                            adjusted_pos_a = pos_a + len(a)
-                            if adjusted_pos_a >= pos_b: return ""
-                            return value[adjusted_pos_a:pos_b]
-                        pinda = (between(s1, 'fact', 'length'))
-                        b= ('\'{},:;')
-                        for char in b:
-                            pinda = pinda.replace(char, "")
+                        s1 = (s1['fact'])
                         pinda = ("""Here is random catfact for you /u/{0}. {1}
 ___
-^^Hello, ^^I'm ^^a ^^bot ^^and ^^this ^^action ^^was ^^performed ^^automatically ^^for ^^questions ^^pm ^^me.""".format(author.name, pinda)) 
+^^Hello, ^^I'm ^^a ^^bot ^^and ^^this ^^action ^^was ^^performed ^^automatically ^^for ^^questions ^^pm ^^me.""".format(author.name, s1)) 
                         print (pinda)
+
+
 
                         n=1
                         if n==1:
